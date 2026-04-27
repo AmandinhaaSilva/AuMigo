55704caa7e9532b42f04e266529b9bf34876f47f
Amanda Freitas da Silva <amandafresilva09@gmail.com>
2026-04-27 12:56:12 -0300

    arrumando

--- conexao/login.php
+++ conexao/login.php
@@ -1,9 +1,3 @@
-<?php
-include("conexao.php");
-
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-
 $sql = "SELECT * FROM usuarios 
         WHERE email = '$email' AND senha = '$senha'";
 
@@ -11,10 +5,11 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 
-    $data = date("Y-m-d H:i:s");
+    $usuario = $result->fetch_assoc();
+    $usuario_id = $usuario['id']; // pega o ID
 
-    $sqlHistorico = "INSERT INTO historico_login (email, data_login)
-                     VALUES ('$email', '$data')";
+    $sqlHistorico = "INSERT INTO historico_login (usuario_id)
+                     VALUES ('$usuario_id')";
 
     $conn->query($sqlHistorico);
 
