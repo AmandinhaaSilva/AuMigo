8af261e35318a2c45076c12b51d50b776c1dcce6
Amanda Freitas da Silva <amandafresilva09@gmail.com>
2026-08-11 13:34:09 -0300

    doações na adm

--- adm/includes/menu.php
+++ adm/includes/menu.php
@@ -4,20 +4,20 @@
         <img src="../logo.png" alt="AuMigo">
     </div>
 
-    <a href="../dashboard.php">🏠 Dashboard</a>
+    <a href="dashboard.php">🏠 Dashboard</a>
 
-    <a href="../animais/listar.php">🐶 Animais</a>
+    <a href="animais/listar.php">🐶 Animais</a>
 
-    <a href="../produtos/listar.php">🛒 Produtos</a>
+    <a href="produtos/listar.php">🛒 Produtos</a>
 
-    <a href="../usuarios/listar.php">👥 Usuários</a>
+    <a href="usuarios/listar.php">👥 Usuários</a>
 
-    <a href="../doacoes/listar.php">❤️ Doações</a>
+    <a href="doacoes/listar.php">❤️ Doações</a>
 
     <a href="#">⚙ Configurações</a>
 
     <hr>
 
-    <a href="../logout.php">🚪 Sair</a>
+    <a href="logout.php">🚪 Sair</a>
 
 </div>
